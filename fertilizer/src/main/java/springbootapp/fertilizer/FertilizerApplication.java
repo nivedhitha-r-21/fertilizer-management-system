3aca2f9d9c91545e4f2149d797d461009fec7338
nivedhitha-r-21 <727723euai082@skcet.ac.in>
2025-09-19 20:14:49 +0530

    adding backend

---
+++ fertilizer/src/main/java/springbootapp/fertilizer/FertilizerApplication.java
@@ -0,0 +1,13 @@
+package springbootapp.fertilizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FertilizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FertilizerApplication.class, args);
+	}
+
+}
---
+++ fertilizer/src/test/java/springbootapp/fertilizer/FertilizerApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootapp.fertilizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FertilizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
